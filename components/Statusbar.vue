d5ce7009e44c3df0177150bae898f442e62a9171
Mike <mikeglopez.91@gmail.com>
2020-05-06 02:45:51 -0500

    Initial commit

---
+++ components/Statusbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <view class="statusbar" :style="{ backgroundColor: color}" />
+</template>
+
+<script>
+export default {
+  props: ['color']
+}
+</script>
+
+<style>
+.statusbar {
+  height: 45px;
+}
+</style>
